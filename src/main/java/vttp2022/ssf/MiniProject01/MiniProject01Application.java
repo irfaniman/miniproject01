06e8d9cf9bdd1d7381e3be83c9c69b3e1bb6528a
Irfan <irfanimanabub@gmail.com>
2022-08-15 16:29:08 +0800

    initial commit

---
+++ src/main/java/vttp2022/ssf/MiniProject01/MiniProject01Application.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.MiniProject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/ssf/MiniProject01/MiniProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.MiniProject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
